cbe0526962c4d601a3d62f604a07fae16ec3500f
Nexaplafrom <nexaplaform@gmail.com>
2025-06-12 18:07:29 +0200

    Desacoplando configuracion para la documentacion de las apis

--- src/main/java/com/nexaplatform/MicAuthApplication.java
+++ src/main/java/com/nexaplatform/MicAuthApplication.java
@@ -1,13 +1,16 @@
 package com.nexaplatform;
 
+import com.nexaplaform.core.api.configuration.OpenApiConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({OpenApiConfig.class})
 public class MicAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicAuthApplication.class, args);
+    }
 
 }
