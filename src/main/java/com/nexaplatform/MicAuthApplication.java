a79a8aa8367027a98ea39c34f47a54fd26d6dfc1
Nexaplafrom <nexaplaform@gmail.com>
2025-07-10 21:48:35 +0200

    Agrgando configuracion para habilitar el core por anotacion

--- src/main/java/com/nexaplatform/MicAuthApplication.java
+++ src/main/java/com/nexaplatform/MicAuthApplication.java
@@ -1,11 +1,11 @@
 package com.nexaplatform;
 
+import com.nexaplaform.core.annotations.EnableCoreServices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
+@EnableCoreServices
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.nexaplatform", "com.nexaplaform.core"})
 public class MicAuthApplication {
 
     public static void main(String[] args) {
