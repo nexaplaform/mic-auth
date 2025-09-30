60ad4d02badb317a2cfe35cabcc791f0cd02934c
Nexaplafrom <nexaplaform@gmail.com>
2025-09-30 19:18:09 +0200

    Cambio de nombre de archivo

--- src/main/java/com/nexaplatform/application/security/CorsRegistryConfiguration.java
+++ src/main/java/com/nexaplatform/application/security/CorsRegistryConfiguration.java
@@ -1,12 +1,20 @@
 package com.nexaplatform.application.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsRegistryConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
